fec78e36dbb783e4cd976b447794efc1f2543b2f
wang <ccna8008@163.com>
2019-06-01 09:25:10 +0800

    0

--- vue/test/index.js
+++ vue/test/index.js
@@ -44,3 +44,33 @@ app4 = new Vue({
         }
     }
 });
+
+var app6 = new Vue({
+    el:'#app6',
+    data: {
+        message:'hello vue!'
+    }
+});
+
+// 定义组件 todo-item
+Vue.component('todo-item', {
+    template: '<li>这是个待办项</li>'
+});
+
+// 先创建组件,再实例化
+Vue.component('todo',{
+    props: ['k2'], 
+    template: '<li>{{ k2.text }}</li>'
+});
+new Vue({
+    el:'#app8',
+    data:{
+        groceryList: [
+            { id:0, text:'123213'},
+            { id:1, text:'23'},
+            { id:2, text:'123'}
+        ]
+    }
+});
+
+
